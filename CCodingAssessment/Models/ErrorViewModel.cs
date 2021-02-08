a23ce592aa33513eab35ed2f62d7d2fcde58038a
joshpters <joshuapetersmail@gmail.com>
2021-02-08 11:43:13 -0500

    Initial commit

---
+++ CCodingAssessment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CCodingAssessment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
